1cb8d1f75abdef731c588d3f48f53e2e441b5537
Ellis Shang <40737228+Shellishack@users.noreply.github.com>
2024-11-21 12:52:53 +0800

    Fix TTL wont apply to encrypted tokens

--- lib/hooks/use-menu-states-context.ts
+++ lib/hooks/use-menu-states-context.ts
@@ -11,13 +11,29 @@ export default function useMenuStatesContext() {
         setMenuStates: Dispatch<SetStateAction<MenuStates>>;
       }
     | undefined = useContext(MenuStatesContext);
-  const { setValue } = useLocalStorage();
+  const { getValue, setValue } = useLocalStorage();
 
   function updateMenuStates(newMenuStates: Partial<MenuStates>) {
     if (context) {
       const updatedStates = { ...context.menuStates, ...newMenuStates };
 
       if (updatedStates.settings) {
+        // Default TTL is set to 14 days
+        if (!updatedStates.settings.ttl) {
+          updatedStates.settings.ttl = 14 * 24 * 60 * 60 * 1000;
+        }
+        // Save settings to local storage
+        const settings = updatedStates.settings;
+        setValue("sttProvider", settings.sttProvider);
+        setValue("llmProvider", settings.llmProvider);
+        setValue("ttsProvider", settings.ttsProvider);
+        setValue("sttModel", settings.sttModel);
+        setValue("llmModel", settings.llmModel);
+        setValue("ttsModel", settings.ttsModel);
+        setValue("isUsePassword", settings.isUsePassword);
+        setValue("isPasswordSet", settings.isPasswordSet);
+        setValue("ttl", settings.ttl); // 14 days
+
         // Do not allow API token editing after password is set
         if (!updatedStates.settings.isPasswordSet) {
           const sttAPIKey = updatedStates.settings.sttAPIKey;
@@ -27,18 +43,15 @@ export default function useMenuStatesContext() {
           setValue("llmAPIKey", llmAPIKey, updatedStates.settings.ttl);
           setValue("ttsAPIKey", ttsAPIKey, updatedStates.settings.ttl);
         }
-
-        // Save settings to local storage
-        const settings = updatedStates.settings;
-        setValue("sttProvider", settings.sttProvider, -1);
-        setValue("llmProvider", settings.llmProvider);
-        setValue("ttsProvider", settings.ttsProvider);
-        setValue("sttModel", settings.sttModel);
-        setValue("llmModel", settings.llmModel);
-        setValue("ttsModel", settings.ttsModel);
-        setValue("isUsePassword", settings.isUsePassword);
-        setValue("isPasswordSet", settings.isPasswordSet);
-        setValue("ttl", settings.ttl);
+        // Only update TTL if it is set
+        else {
+          const encryptedSttAPIKey = getValue<string>("sttAPIKey");
+          const encryptedLlmAPIKey = getValue<string>("llmAPIKey");
+          const encryptedTtsAPIKey = getValue<string>("ttsAPIKey");
+          setValue("sttAPIKey", encryptedSttAPIKey, updatedStates.settings.ttl);
+          setValue("llmAPIKey", encryptedLlmAPIKey, updatedStates.settings.ttl);
+          setValue("ttsAPIKey", encryptedTtsAPIKey, updatedStates.settings.ttl);
+        }
       } else {
         // Reset all settings
         setValue("sttProvider", undefined);
