a75f1a06da1ab4cb8c7394d9407bb36dca36e8bb
Ellis Shang <40737228+Shellishack@users.noreply.github.com>
2024-12-13 22:18:30 +0800

    Add release electron build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.chisel.app',
-  appName: 'Chisel Editor',
-  webDir: 'out-next'
+  appId: "com.chisel.app",
+  appName: "Chisel Editor",
+  webDir: "out-next",
+  android: {
+    buildOptions: {
+      signingType: "apksigner",
+    },
+  },
 };
 
 export default config;
