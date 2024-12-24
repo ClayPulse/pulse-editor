5b7677f6b3b10681729eb3b92fa62a1a86bde618
Ellis Shang <40737228+Shellishack@users.noreply.github.com>
2024-12-24 09:26:55 +0800

    Organize monorepo -- move capacitor into mobile folder

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,12 +3,13 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.pulse.app",
   appName: "Pulse Editor",
-  webDir: "build/next",
+  webDir: "../build/next",
   android: {
     buildOptions: {
       signingType: "apksigner",
     },
-  },
+    path: "./android",
+  }, 
 };
 
 export default config;
